f60d3f1d39932164fddb8493b991d9cdd9ee5947
Guy Benoit <guy.benoit@novaccess.ch>
2025-02-14 22:30:17 +0100

    Challenge implementation and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InternationalizationService } from './shared/services/internationalization.service';
+import { ErrorToastComponent } from './shared/components/error-toast/error-toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ErrorToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'challenge-frontend';
+
+  constructor(private internationalizationService: InternationalizationService) {
+    this.internationalizationService.initialize();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SigningRequestComponent } from './signing-request/signing-request.component';
+import { AuthorizeGuard } from './shared/services/authorize-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: SigningRequestComponent, canActivate : [AuthorizeGuard] },
+];
